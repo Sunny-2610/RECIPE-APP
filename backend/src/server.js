d9c58dfbff49a5c684cbe2bef784bcd3f426893c
Sunny-2610 <sunnysinha9884@gmail.com>
2026-07-01 18:31:47 +0530

    setup up the backend file

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { ENV } from "./config/env.js";
+const app = express()
+const PORT = ENV.PORT || 5001 ;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
+
+app.listen(5001, () =>{
+    console.log("Server is running on port 5001")
+})
